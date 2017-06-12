1ee64f82173024d1626df9a1527b105c62fa625f
JSJSPSHT1922 <jordanarekterate1922@gmail.com>
2017-06-12 13:26:38 +0700

    Update adsantilok-komangid.js

--- adsantilok-komangid.js
+++ adsantilok-komangid.js
@@ -37,7 +37,7 @@ function jAntiBlock() {
     jQuery(function () {
         jQuery(document).ready(function () {
             setTimeout("CekBlok();", 1000);
-            jQuery("body").append('<div id="energysaving"><br></br><br></br><img src="https://lh3.googleusercontent.com/-1wnZJX2L4YI/VvputzGCoXI/AAAAAAAAD8U/xD93WB_Fl7sRO5aBgXRGplJV3SD7rgd9wCCo/s512-Ic42/chinoads.png" height="350" width="350" alt="Stop-AdBlock" /><div id="copyrightOnlineLeaf">Copyright 2016 <font style="color: #666666;"> LinuxSec</font> - All rights reserved</div></div>');
+            jQuery("body").append('<div id="energysaving"><br></br><br></br><img src="https://4.bp.blogspot.com/-EzI3zbTsyeI/WT4z16nooII/AAAAAAAAAds/pe9Vlipp_EUhI9iXBdVUR9KmFpRyHFJtgCLcB/s1600/chinoads.png" height="350" width="350" alt="Stop-AdBlock" /><div id="copyrightOnlineLeaf">Copyright 2016 <font style="color: #666666;"> LinuxSec</font> - All rights reserved</div></div>');
         });
     });
 }
